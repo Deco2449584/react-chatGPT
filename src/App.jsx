0895ba96b209db50c43ffb9ecd61d894d1abcec3
Daniel <deco2449584@gmail.com>
2023-06-29 22:43:12 -0500

    se realiza el componente signin con estilos scss

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import SigninForm from "./components/SigninForm";
+
+const App = () => {
+  return <SigninForm />;
+};
+
+export default App;
---
+++ src/components/SigninForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Card from "./Card";
+import "../styles/SigninForm.scss";
+
+const SigninForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lógica de autenticación
+  };
+
+  return (
+    <div className="container">
+      <Card>
+        <form onSubmit={handleSubmit} className="signin-card">
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
+          <button type="submit">Sign In</button>
+          <div className="signin-links">
+            <a href="#">Forgot Password</a>
+            <span> | </span>
+            <a href="#">Sign Up</a>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default SigninForm;
